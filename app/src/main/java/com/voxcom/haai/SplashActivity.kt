e1ceb3fd056ff9d1ecf13bbb84da29567cffd717
Midhun1618 <midhumidhun342@gmail.com>
2026-03-16 15:15:25 +0530

    add Activities : basic user flow

--- app/src/main/java/com/voxcom/haai/SplashActivity.kt
+++ app/src/main/java/com/voxcom/haai/SplashActivity.kt
@@ -8,11 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         }
         val nextpage = findViewById<Button>(R.id.nextpage)
         nextpage.setOnClickListener {
-            startActivity(Intent(this, SymptomsActivity::class.java))
+            startActivity(Intent(this, OnboardingActivity::class.java))
         }
     }
 }
